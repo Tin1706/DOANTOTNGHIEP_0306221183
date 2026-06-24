e391fb0677ee9dcb66f63bed1e9e00f1fc6c8e8c
= <tinhuytrg3172@gmail.com>
2026-06-24 15:55:05 +0700

    Lần commit cuối cùng của API

--- doantotnghiep/lib/constant.dart
+++ doantotnghiep/lib/constant.dart
@@ -1,3 +1,4 @@
-class AppConstant {
-    static const String address = "https://diabetic-api-backend-0.onrender.com/";
+class AppConstant{
+    static const String address = "https://diabetes-backend-ntij.onrender.com/";
 }
+   
